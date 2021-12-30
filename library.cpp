6b5a8acc95caa68bb03cfb6aa351f60fc668b803
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-12-30 02:14:36 +0100

    Add move mouse function

--- library.cpp
+++ library.cpp
@@ -1,5 +1,4 @@
 #include <windows.h>
-#include <chrono>
 
 void sendKeyPress(bool down, int key) {
     INPUT input;
@@ -34,6 +33,26 @@ extern void __cdecl sendKey(bool down, int key) {
     sendKeyPress(down, key);
 }
 
+extern "C" {
+__declspec(dllexport) void __cdecl moveMouse(int, int);
+}
+
+extern void __cdecl moveMouse(int x, int y) {
+    int screenWidth = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+    // Move mouse
+    INPUT input = {0};
+    input.type = INPUT_MOUSE;
+    input.mi.dx = (x * 65535) / (screenWidth - 1);
+    input.mi.dy = (y * 65535) / (screenHeight - 1);
+    input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_VIRTUALDESK | MOUSEEVENTF_ABSOLUTE;
+    input.mi.mouseData = 0;
+    input.mi.time = 0;
+    input.mi.dwExtraInfo = 0;
+    ::SendInput(1, &input, sizeof(INPUT));
+    ::ZeroMemory(&input, sizeof(INPUT));
+}
 
 extern "C" {
 __declspec(dllexport) void __cdecl clickPos(bool, int, int);
